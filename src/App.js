0982922e335f65836c72d3bf43b9ef0213883caf
anhnguyen6535 <anhnguyen6535@gmail.com>
2022-04-16 02:49:16 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
+  const [biHash, setBiHash] = useState("")
+  const [hexHash, setHexHash] = useState("")
+  const refInput = useRef()
+
+  const clickHandler = (e) => {
+    e.preventDefault();
+    const text = refInput.current.value
+    if(text != ""){
+
+      var SHA256 = require("crypto-js/sha256");
+      var hexToBinary = require('hex-to-binary');
+  
+      var hashString = SHA256(text).toString()
+      setHexHash(hashString)
+  
+      var binaryHash = hexToBinary(hashString)
+  
+      setBiHash(binaryHash)   
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='hero'>
+      <div className='container'>
+        <div className='input'>
+          <h3 className='title'>Type your input below</h3>
+          <form onSubmit={clickHandler}>
+            <input type="text" className="textInput" ref={refInput}/>
+            <input type="submit" className="convert" value="SHA256 Encrypt" onClick={clickHandler} onMouseDown={e => e.preventDefault()}/>
+          </form>
+        </div>
+
+        <div className='output'>
+          <div className='hashOutput'>
+            <h3 className='title'>Hexadecimal hash</h3>
+            <p className='hex-hash'>{hexHash}</p>
+          </div>
+
+          <div className='hashOutput'>
+            <h3 className='title'>Binary hash</h3>
+            <p className='bi-hash'>{biHash}</p>
+          </div>
+          
+        </div>
+      </div>
     </div>
   );
 }
